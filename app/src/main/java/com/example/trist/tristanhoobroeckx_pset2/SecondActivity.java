a05bb678d55c61f58c8f7ec58fe28892e4a26e80
Tristanhx <tristanhx@hotmail.nl>
2017-04-18 14:33:37 +0200

    Added counter and hints

--- app/src/main/java/com/example/trist/tristanhoobroeckx_pset2/SecondActivity.java
+++ app/src/main/java/com/example/trist/tristanhoobroeckx_pset2/SecondActivity.java
@@ -1,17 +1,21 @@
 package com.example.trist.tristanhoobroeckx_pset2;
 
 import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public class SecondActivity extends AppCompatActivity {
 
     EditText text;
-    Story storyBuilder = new Story();
+    Story storyBuilder;
+    TextView counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +23,31 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         text = (EditText)findViewById(R.id.edittext);
-        text.setHint(storyBuilder.getNextPlaceholder());
+        counter = (TextView) findViewById(R.id.counter);
+
+
+        if (savedInstanceState != null){
+            storyBuilder = (Story) savedInstanceState.getSerializable("text");
+            text.setHint(storyBuilder.getNextPlaceholder());
+            counter.setText(Integer.toString(storyBuilder.getPlaceholderRemainingCount()));
+        }
+        else {
+            try {
+                storyBuilder = new Story(getAssets().open("madlib0_simple.txt"));
+                text.setHint(storyBuilder.getNextPlaceholder());
+                counter.setText(Integer.toString(storyBuilder.getPlaceholderRemainingCount()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("text", storyBuilder);
+//        outState.putString("hint", (String) text.getHint());
+//        outState.putInt("counter", storyBuilder.getPlaceholderRemainingCount());
     }
 
     protected void gotoThird(){
@@ -32,6 +60,8 @@ public class SecondActivity extends AppCompatActivity {
         if (storyBuilder.isFilledIn() != true){
             String word = text.getText().toString();
             storyBuilder.fillInPlaceholder(word);
+            text.setHint(storyBuilder.getNextPlaceholder());
+            counter.setText(Integer.toString(storyBuilder.getPlaceholderRemainingCount()));
         }
         else
             gotoThird();
